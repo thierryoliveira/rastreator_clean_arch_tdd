44ec9840f47ea5f2849e77fd4b73a640b7820d36
Thierry Oliveira <thierry.ifsp@gmail.com>
2021-12-09 23:52:44 -0300

    feat: add usecase abstract

---
+++ lib/app/core/usecase/errors/failures.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {}
+
+class ServerFailure extends Failure {
+  @override
+  List<Object> get props => [];
+}
--- lib/app/core/usecase/usecase.dart
+++ lib/app/core/usecase/usecase.dart
@@ -1,1 +1,12 @@
-abstract class UseCase<Output, Input> {}
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:rastreator/app/core/usecase/errors/failures.dart';
+
+abstract class UseCase<Output, Input> {
+  Future<Either<Failure, Output>> call(Input params);
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object> get props => [];
+}
