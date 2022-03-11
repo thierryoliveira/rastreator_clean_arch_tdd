17d46803102c2152aba1f2292a1f678f1a7aaf98
Thierry Oliveira <thierry.oliveira@compasso.com.br>
2022-03-11 11:34:11 -0300

    feat: add blocbuilder to manage state on homescreen

--- lib/app/presentation/package/home.screen.dart
+++ lib/app/presentation/package/home.screen.dart
@@ -2,10 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:rastreator/app/core/colors.dart';
+import 'package:rastreator/app/presentation/global/widgets/custom_loading.dart';
 import 'package:rastreator/app/presentation/package/widgets/no_package.screen.dart';
 import 'package:rastreator/app/presentation/package/widgets/package_track_info.screen.dart';
 
+import '../../core/dependency_injection/injection_container.dart';
 import 'bloc/package_bloc.dart';
+import 'widgets/dialog_add_package.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -13,33 +16,30 @@ class HomeScreen extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return Scaffold(
         //TODO* REFACTOR HERE
-        // floatingActionButton: Visibility(
-        //       visible: controller.events.length == 0,
-        //       child: Padding(
-        //         padding: EdgeInsets.only(
-        //             bottom: size.height * .03, right: size.width * .03),
-        //         child: FloatingActionButton(
-        //           backgroundColor: kGreenColor,
-        //           onPressed: () {
-        //             showDialogToAddPackage();
-        //           },
-        //           child: Icon(
-        //             Icons.add,
-        //           ),
-        //         ),
-        //       ),
-        //     ),
+        floatingActionButton: BlocBuilder<PackageBloc, PackageState>(
+          builder: (context, state) {
+            if (state is LoadedState) return Container();
+            return Padding(
+              padding: EdgeInsets.only(
+                  bottom: size.height * .03, right: size.width * .03),
+              child: FloatingActionButton(
+                backgroundColor: kGreenColor,
+                onPressed: () {
+                  showDialogAddPackage(context: context);
+                },
+                child: Icon(
+                  Icons.add,
+                ),
+              ),
+            );
+          },
+        ),
         backgroundColor: kDarkBlueColor,
         body: SafeArea(
           child: BlocBuilder<PackageBloc, PackageState>(
             builder: (context, state) {
               final bloc = BlocProvider.of<PackageBloc>(context);
-              if (state is LoadingState)
-                return Center(
-                  child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(kOrangeColor),
-                  ),
-                );
+              if (state is LoadingState) return CustomLoading();
               if (state is LoadedState)
                 return PackageTrackInfoScreen(
                   trackId: bloc.trackId,
@@ -51,55 +51,4 @@ class HomeScreen extends StatelessWidget {
           ),
         ));
   }
-
-//   showDialogToAddPackage() {
-//     final _formKey = GlobalKey<FormState>();
-
-//     Get.defaultDialog(
-//         onWillPop: () => controller.clearEvents(),
-//         contentPadding: EdgeInsets.fromLTRB(
-//             size.width * .05, 0, size.width * .05, size.height * .02),
-//         backgroundColor: kOffwhiteColor,
-//         title: 'Rastreie um novo pacote',
-//         titleStyle: TextStyle(color: kGreyColor, fontWeight: FontWeight.w500),
-//         content: Form(
-//           key: _formKey,
-//           child: Column(
-//             children: [
-//               CustomTextField(
-//                 label: 'Apelido para o pacote',
-//                 textController: controller.txtPackageName,
-//                 isRequiredValidation: true,
-//               ),
-//               CustomTextField(
-//                 label: 'Código de rastreio',
-//                 textController: controller.txtTrackId,
-//                 isRequiredValidation: true,
-//               ),
-//               Container(
-//                   margin: EdgeInsets.only(top: size.height * .02),
-//                   width: size.width,
-//                   child: ElevatedButton(
-//                     onPressed: () {
-//                       if (_formKey.currentState!.validate()) {
-//                         controller.getTrackInfo(controller.txtTrackId.text);
-//                         controller.trackId.value = controller.txtTrackId.text;
-//                         controller.packageName.value =
-//                             controller.txtPackageName.text;
-//                       }
-//                     },
-//                     child: Text(
-//                       'Adicionar',
-//                       style:
-//                           TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-//                     ),
-//                     style: ElevatedButton.styleFrom(
-//                         primary: kGreenColor,
-//                         shape: RoundedRectangleBorder(
-//                             borderRadius: BorderRadius.circular(15))),
-//                   ))
-//             ],
-//           ),
-//         ));
-//   }
 }
